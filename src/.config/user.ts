a1593d10e2d8bef720f4b3be85dfebd24c6a1b5c
pengchujin <2093959501@qq.com>
2025-08-26 12:00:25 +0800

    update config

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,81 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-	site: { title: "酱紫表" },
-	seo: { twitter: "@pengchujin" },
+	site: {
+    title: '酱紫表',
+    subtitle: '复杂的世界里一个就足够了',
+    author: '酱紫表',
+    description: '酱紫表的博客',
+    website: 'https://qust.me/',
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      {
+        name: 'twitter',
+        href: 'https://x.com/pengchujin',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+      'Proudly published with <a target="_blank" href="https://astro.build/">Astro</a>',
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'zh-cn',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '@pengchujin',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
 }
